2a7d60da5835d7c00cb095d6196d3b7bd1ea29ad
Tori033 <71142218+sunnypiase@users.noreply.github.com>
2024-04-10 09:34:32 +0300

    progress

--- DataProvider.py
+++ DataProvider.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from typing import Optional
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
 
 class DataProvider:
     def __init__(self, file_path: str) -> None:
@@ -19,6 +21,14 @@ class DataProvider:
         # Reverse the DataFrame and reset index
         self.df_raw = self.df_raw.iloc[::-1].reset_index(drop=True)
 
+        # Calculate technical indicators
+        self.df_raw['rsi'] = self.calculate_rsi(self.df_raw['close'])
+        self.df_raw['ema200'] = self.df_raw['close'].ewm(span=200, adjust=False).mean()
+        self.df_raw['ema50'] = self.df_raw['close'].ewm(span=50, adjust=False).mean()
+        self.df_raw['ema12'] = self.df_raw['close'].ewm(span=12, adjust=False).mean()
+        self.df_raw['macd'] = self.calculate_macd(self.df_raw['close'])
+        self.df_raw['atr'] = self.calculate_atr(self.df_raw)
+
         # Log transformation
         self.df_raw['volume'] = np.log(self.df_raw['volume'] + 1)
         self.df_raw['volume_from'] = np.log(self.df_raw['volume_from'] + 1)
@@ -27,7 +37,36 @@ class DataProvider:
         # Normalize the data
         scaler = StandardScaler()
         normalized_data = scaler.fit_transform(self.df_raw)
-        self.df_normalized = pd.DataFrame(normalized_data, columns=self.df_raw.columns)
+
+        # Apply Min-Max scaling
+        min_max_scaler = MinMaxScaler()
+        min_max_normalized_data = min_max_scaler.fit_transform(normalized_data)
+        self.df_normalized = pd.DataFrame(min_max_normalized_data, columns=self.df_raw.columns)
+
+    @staticmethod
+    def calculate_rsi(series, period=14):
+        delta = series.diff()
+        gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
+        loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
+        rs = gain / loss
+        return 100 - (100 / (1 + rs))
+
+    @staticmethod
+    def calculate_macd(series, fast_period=12, slow_period=26, signal=9):
+        fast_ema = series.ewm(span=fast_period, adjust=False).mean()
+        slow_ema = series.ewm(span=slow_period, adjust=False).mean()
+        macd = fast_ema - slow_ema
+        signal_line = macd.ewm(span=signal, adjust=False).mean()
+        return macd - signal_line
+
+    @staticmethod
+    def calculate_atr(df, period=14):
+        high_low = df['high'] - df['low']
+        high_close = np.abs(df['high'] - df['close'].shift())
+        low_close = np.abs(df['low'] - df['close'].shift())
+        ranges = pd.concat([high_low, high_close, low_close], axis=1)
+        true_range = ranges.max(axis=1)
+        return true_range.rolling(window=period).mean()
 
     def get_raw_data(self) -> pd.DataFrame:
         if self.df_raw is not None:
@@ -40,3 +79,27 @@ class DataProvider:
             return self.df_normalized
         else:
             raise ValueError("Normalized data not available.")
+        
+    def plot_histograms(self) -> None:
+        if self.df_raw is not None:
+            num_columns = len(self.df_raw.columns)
+            fig, axes = plt.subplots(num_columns, 1, figsize=(10, num_columns * 4))
+
+            for i, col in enumerate(self.df_raw.columns):
+                sns.histplot(self.df_raw[col], ax=axes[i], kde=True)
+                axes[i].set_title(f'Histogram of {col}')
+
+            plt.tight_layout()
+            plt.show()
+
+    def plot_histograms_norm(self) -> None:
+        if self.df_normalized is not None:
+            num_columns = len(self.df_normalized.columns)
+            fig, axes = plt.subplots(num_columns, 1, figsize=(10, num_columns * 4))
+
+            for i, col in enumerate(self.df_normalized.columns):
+                sns.histplot(self.df_normalized[col], ax=axes[i], kde=True)
+                axes[i].set_title(f'Histogram norm of {col}')
+
+            plt.tight_layout()
+            plt.show()
--- TraderEnvNormilized.py
+++ TraderEnvNormilized.py
@@ -1,95 +1,81 @@
 import numpy as np
 import gymnasium as gym
 from gymnasium.spaces import Discrete, Box
-import pandas as pd
 
 class TraderEnvNormalized(gym.Env):
+
     MAX_TIME_WITHOUT_TRADE = 1500
     RISK_FREE_RATE = 0.02
-    RISK_FREE_RATE_PER_STEP = (1 + RISK_FREE_RATE) ** (1 / 525600) - 1  # Compounded for each minute
+    RISK_FREE_RATE_PER_STEP = (1 + RISK_FREE_RATE) ** (1 / 525600) - 1
+    COMMISSION_RATE = 0.05 / 100
+    LONG_POSITION = 1
+    SHORT_POSITION = -1
+    NO_POSITION = 0
+    STOP_LOSS_THRESHOLD = 0.05  # 5% stop-loss
 
-    def __init__(self, df_raw, df_normalized, trade_size_dollars=5_000, initial_capital=10_000, render_mode='human'):
-        super(TraderEnvNormalized, self).__init__()
-        self.render_mode = render_mode
+
+    def __init__(self, df_raw, df_normalized, trade_size_dollars=1_000, initial_capital=10_000, save_history=False, render_mode='human'):
+        super().__init__()
         self.action_space = Discrete(4)  # Buy, Sell, Close, Hold
-        self.observation_space = Box(low=-np.inf, high=np.inf, shape=(8,))
+        self.observation_space = Box(low=-np.inf, high=np.inf, shape=(15,))
+        self.render_mode = render_mode
         self.df_raw = df_raw
         self.df_normalized = df_normalized
         self.trade_size_dollars = trade_size_dollars
-        self.commission = self.calculate_commission(trade_size_dollars)
         self.initial_capital = initial_capital
-        
+        self.segment_start = 0
+        self.segment_end = 0
+        self.current_offset = 0
+        self.trade_details = []
+        self.save_history = save_history
         self.reset()
 
-    def step(self, action):
-        self.current_step += 1
-        self.current_price = self.df_raw['close'].iloc[self.current_step]
-        profitLose = self.process_action(action, self.current_price)
-        self.state = self._next_observation()
-        self.sharp_ratio = self.calculate_sharpe_ratio()
-        if self.current_capital < self.trade_size_dollars or  self.sharp_ratio< -20:
-            self.stop = True
-
-        reward = 1 + profitLose #self.calculate_reward()
-
-        done = self.current_step >= len(self.df_raw) - 1 or self.stop
-        info = {'current_capital': self.current_capital, 'sharp_ratio': self.sharp_ratio, 'current_step': self.current_step,
-                'trades_amount': len(self.trade_history)} if done else {}
-        
-        return self.state, reward, done, done, info
-
-    def calculate_commission(self, trade_size_dollars):
-        commission_rate = 0.05 / 100
-        return trade_size_dollars * commission_rate
-
     def reset(self, **kwargs):
-        self.current_step = 0
+        self._segment_data()
+        self.current_step = -1
+        self.profit_lose= 0
+        self.current_offset = self.segment_start
+        self.close_prices = self.df_raw['close'].values
+        self.current_price = self.close_prices[self.current_offset]
         self.current_position = None
         self.position_changed = False
         self.current_capital = self.initial_capital
         self.trade_history = []
-        self.current_price = self.df_raw['close'].iloc[self.current_step]
         self.stop = False
-        self.sharp_ratio = 0
+        self.sharpe_ratio = self._calculate_sharpe_ratio()
+        self.commission = self.trade_size_dollars * self.COMMISSION_RATE
+        self.normalized_data = self.df_normalized.values
         self.state = self._next_observation()
+        
         return self.state, None
+        
+    def step(self, action):
+        self.current_step += 1
+        self.state = self._next_observation()
+        actual_index = self.current_step + self.current_offset
+        self.current_price = self.close_prices[actual_index]
+        self._process_action(action)        
+        self.sharpe_ratio = self._calculate_sharpe_ratio()
+        self.stop = self._check_stop_condition()
 
-    def calculate_reward(self):
-            simple_return = self.trade_history[-1] if self.trade_history else 0
-            unrealized_pnl = self.calculate_profit_loss(self.current_price) if self.current_position else 0
-            trade_risk = self.calculate_trade_risk()
-            holding_reward = self.calculate_holding_reward()
+        reward = self._calculate_reward()
+        if self.stop: 
+            reward -= 1
 
-            alpha, beta, gamma = 0.5, 0.3, 0.2
-            reward = alpha * simple_return + beta * unrealized_pnl - gamma * trade_risk + holding_reward
-            return reward
+        # Formatting the state for readability
+        # formatted_state = ', '.join([f"{value:.6f}" for value in self.state])
+        # print(f"Step: {self.current_step}")
+        # print(f"Current Price: {self.current_price:.6f}")
+        # print(f"Reward: {reward}")
+        # print(f"Profit/Loss: {self._calculate_profit_loss():.6f}")
+        # print(f"Sharpe Ratio: {self.sharpe_ratio:.6f}")
+        # print(f"State: [{formatted_state}]")
+        # print()
+        done = actual_index == self.segment_end - 1 or self.stop
+        info = self._get_info(done)
+            
+        return self.state, reward, done, done, info
 
-    def calculate_trade_risk(self):
-        window_size = 10
-        if len(self.trade_history) < window_size:
-            return 0
-        recent_returns = self.trade_history[-window_size:]
-        return np.std(recent_returns)
-
-    def calculate_holding_reward(self):
-        if not self.current_position or not self.position_open_step:
-            return 0
-
-        holding_duration = self.current_step - self.position_open_step
-        min_holding_duration = 20  # Minimum desired holding duration
-        max_holding_duration = 256  # Maximum desired holding duration
-
-        # Penalty for holding a position for too short a time
-        if holding_duration < min_holding_duration:
-            return -0.01 * (min_holding_duration - holding_duration)
-
-        # Penalty for holding a position for too long
-        elif holding_duration > max_holding_duration:
-            return -0.01 * (holding_duration - max_holding_duration)
-
-        # No penalty if within the desired holding duration range
-        else:
-            return 0
 
     def render(self, mode='human', close=False):
         if self.position_changed:
@@ -99,78 +85,120 @@ class TraderEnvNormalized(gym.Env):
                 time_in_position = self.current_step - self.position_open_step if self.position_open_step is not None else 0
                 last_trade_return = self.trade_history[-1] if self.trade_history else 0
                 print(f"Position Closed: Exit Price: {self.current_price}, Close Step: {self.current_step}, Time in Position: {time_in_position}, Return from Last Trade: {last_trade_return}")
-            self.position_changed = False 
+            self.position_changed = False
         # else:
-        #     print(f"Current Step: {self.current_step}, Current Price: {self.current_price}, Current Capital: {self.current_capital}, sharp_ratio: {self.calculate_sharpe_ratio()}")
-
+        #     print(f"Current Step: {self.current_step}, Current Price: {self.current_price}, Current Capital: {self.current_capital}, sharp_ratio: {self.sharpe_ratio}")
     
+    
+    def _calculate_reward(self):
+        reward = 1
+        reward +=  self._calculate_profit_loss()/(self.trade_size_dollars * self.STOP_LOSS_THRESHOLD)
+        reward += self.sharpe_ratio / 20
+        return reward
+
+    def _segment_data(self):
+        # segment_size = len(self.df_raw) // 3
+        # self.segment_start = np.random.randint(0, len(self.df_raw) - segment_size)
+        # self.segment_end = self.segment_start + segment_size
+        segment_size = len(self.df_raw)
+        self.segment_start = 0
+        self.segment_end = self.segment_start + segment_size
+
     def _next_observation(self):
-        market_observation = self.df_normalized.iloc[self.current_step]
-        position_state = 1 if self.current_position and self.current_position["type"] == "long" else \
-                        -1 if self.current_position and self.current_position["type"] == "short" else 0
-        return np.append(market_observation, position_state)
+        actual_index = self.current_step + self.current_offset
+        market_observation = self.normalized_data[actual_index]
+        position_state = self._get_position_state()
 
-    def process_action(self, action, current_price):
+        return np.append(market_observation, [position_state, self._calculate_profit_loss()/(self.trade_size_dollars * self.STOP_LOSS_THRESHOLD)])
+
+    def _process_action(self, action):
         if action == 1:  # Buy
-            self.open_position("long", current_price)
-            return 0
+            self._open_position("long")
         elif action == 2:  # Sell
-            self.open_position("short", current_price)
-            return 0
+            self._open_position("short")
         elif action == 3:  # Close position
-            return self.close_position(current_price)
-        return self.update_position_status(current_price)
-    
-    def open_position(self, position_type, current_price):
-        if not self.current_position and self.current_capital >= self.trade_size_dollars:
-            contracts = self.trade_size_dollars / current_price
-            self.current_position = {"type": position_type, "entry_price": current_price, "contracts": contracts}
-            self.position_changed = True
-            self.position_open_step = self.current_step  # Set the opening step
+            self._close_position()
+        if self.current_position: # Check stop loss
+            if self.current_position["type"] == "long":
+                if self.current_price < self.current_position["entry_price"] * (1 - self.STOP_LOSS_THRESHOLD):
+                    self._close_position()
+            elif self.current_position["type"] == "short":
+                if self.current_price > self.current_position["entry_price"] * (1 + self.STOP_LOSS_THRESHOLD):
+                    self._close_position()
+        return 0
 
-    def close_position(self, current_price):
-        if self.current_position:
-            profit_loss = self.calculate_profit_loss(current_price)
+    def _open_position(self, position_type):
+        if not self.current_position and self.current_capital >= self.trade_size_dollars:
+            contracts = self.trade_size_dollars / self.current_price
+            self.current_position = {"type": position_type, "entry_price": self.current_price, "contracts": contracts}
+            self.position_changed = True
+            self.position_open_step = self.current_step
+            if self.save_history:
+                self.trade_details.append({
+                    'step': self.current_step + self.current_offset,
+                    'type': position_type,
+                    'action': 'open',
+                    'price': self.current_price,
+                    'capital': self.current_capital
+                })
+
+    def _close_position(self):
+        if self.current_position: 
+            profit_loss = self._calculate_profit_loss()
             profit_loss -= self.commission
             self.trade_history.append(profit_loss)
             self.current_capital += profit_loss
+            if self.save_history:
+                self.trade_details.append({
+                    'step': self.current_step + self.current_offset,
+                    'type': self.current_position["type"],
+                    'action': 'close',
+                    'price': self.current_price,
+                    'capital': self.current_capital
+                })
             self.current_position = None
             self.position_changed = True
-            
-            return profit_loss
-        return 0
 
-
-    def calculate_profit_loss(self, exit_price):
+    def _calculate_profit_loss(self):
         if not self.current_position:
             return 0
         contracts = self.current_position["contracts"]
-        profit_loss = (exit_price - self.current_position["entry_price"]) * contracts
+        profit_loss = (self.current_price - self.current_position["entry_price"]) * contracts
         return profit_loss if self.current_position["type"] == "long" else -profit_loss
 
-    def calculate_sharpe_ratio(self):
-        risk_free_rate_per_step = TraderEnvNormalized.RISK_FREE_RATE_PER_STEP
-        risk_free_return_for_trade_size = risk_free_rate_per_step * self.trade_size_dollars * self.current_step
-
+    def _calculate_sharpe_ratio(self):
         if not self.trade_history:
-            # print("\nAdjusted Total Returns: N/A, Returns Std: N/A, Risk-Free Return for Trade Size: {:.3f}, Steps: {}, Sharpe Ratio: {:.3f}".format(risk_free_return_for_trade_size, self.current_step, -risk_free_return_for_trade_size))
-            return -risk_free_return_for_trade_size
+            return 0
 
         total_returns = sum(self.trade_history)
         returns_std = np.std(self.trade_history)
 
         if returns_std == 0:
-            # print("\nAdjusted Total Returns: N/A, Returns Std: 0.000, Risk-Free Return for Trade Size: {:.3f}, Steps: {}, Sharpe Ratio: {:.3f}".format(risk_free_return_for_trade_size, self.current_step, -risk_free_return_for_trade_size))
-            return -risk_free_return_for_trade_size
+            return 0
 
-        adjusted_total_returns = total_returns - risk_free_return_for_trade_size
-        # print("\nAdjusted Total Returns: {:.3f}, Returns Std: {:.3f}, Risk-Free Return for Trade Size: {:.3f}, Steps: {}, Sharpe Ratio: {:.3f}".format(adjusted_total_returns, returns_std, risk_free_return_for_trade_size, self.current_step, adjusted_total_returns / returns_std))
+        risk_free_return = self.RISK_FREE_RATE_PER_STEP * self.trade_size_dollars * self.current_step
+        adjusted_total_returns = total_returns - risk_free_return
         return adjusted_total_returns / returns_std
 
-    def update_position_status(self, current_price):
+    def _check_stop_condition(self):
+        return self.current_capital < self.trade_size_dollars or self.sharpe_ratio < -20
+
+    def _get_info(self, done):
+        if done:
+            return {
+                'current_capital': self.current_capital,
+                'sharpe_ratio': self.sharpe_ratio, 
+                'current_step': self.current_step,
+                'trades_amount': len(self.trade_history),
+                'trade_details': self.trade_details
+            }
+        return {}
+
+    def _get_position_state(self):
         if self.current_position:
-            if self.current_position["type"] == "long" and current_price < self.current_position["entry_price"] * 0.95:
-                return self.close_position(current_price)
-            elif self.current_position["type"] == "short" and current_price > self.current_position["entry_price"] * 1.05:
-                return self.close_position(current_price)
-        return 0
+            if self.current_position["type"] == "long":
+                return self.LONG_POSITION
+            elif self.current_position["type"] == "short":
+                return self.SHORT_POSITION
+        return self.NO_POSITION
+
